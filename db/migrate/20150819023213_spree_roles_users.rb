7ffe7f9a840f35c5cb2232c10b0a0a3a2d8bef26
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-08-18 21:42:58 -0500

    Delete migration

--- db/migrate/20150819023213_spree_roles_users.rb
+++
@@ -1,9 +0,0 @@
-class SpreeRolesUsers < ActiveRecord::Migration
-  create_table :spree_roles_users, :id => false do |t|
-      t.references :role
-      t.references :user
-    end
-
-  add_index :spree_roles_users, [:role_id], :name => 'index_spree_roles_users_on_role_id'
-  add_index :spree_roles_users, [:user_id], :name => 'index_spree_roles_users_on_user_id'
-end
