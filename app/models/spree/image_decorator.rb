c5d6d678fd692a49487cfcff8f6258980fcff6dc
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-08-26 14:11:06 -0500

    Add blog and options values images with s3

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Image.class_eval do
   belongs_to :cms_field, class_name: "Spree::CmsField"
   belongs_to :ambassador, class_name: "Spree::Ambassador"
-  belongs_to :option_value, class_name: "Spree::OptionValue"
+  belongs_to :blog_entry, class_name: "Spree::BlogEntry"
 end
--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,12 +1,36 @@
 Spree::OptionValue.class_eval do
-  has_many :images, -> { order(:position) }, as: :viewable, dependent: :destroy, class_name: "Spree::Image"
+  has_attached_file(
+    :image,
+    s3_credentials: {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    bucket:            ENV['S3_BUCKET_NAME']
+    },
 
-  accepts_nested_attributes_for :images, :allow_destroy => true, :reject_if => :all_blank
+    storage:        :s3,
+    s3_headers:     { "Cache-Control" => "max-age=31557600" },
+    s3_protocol:    "https",
+    bucket:         ENV['S3_BUCKET_NAME'],
+    url:            ":s3_domain_url",
+
+    styles: {
+      mini:     "48x48>",
+      small:    "100x100>",
+      product:  "240x240>",
+      large:    "600x600>"
+    },
+
+    path:           "/:class/:id/:style/:basename.:extension",
+    default_url:    "/:class/:id/:style/:basename.:extension",
+    default_style:  "product"
+  )
+  
+  validates_attachment_content_type :image, content_type: /\Aimage/
 
   def has_image?
     image_file_name && !image_file_name.empty?
   end
 
   default_scope { order("#{quoted_table_name}.position") }
-  scope :for_product, lambda { |product| select("DISTINCT #{table_name}.*").where("spree_option_value_variants.variant_id IN (?)", product.variant_ids).joins(:variants) }
+  scope :for_product, lambda { |product| select("DISTINCT #{table_name}.*").where("spree_option_values_variants.variant_id IN (?)", product.variant_ids).joins(:variants) }
 end
