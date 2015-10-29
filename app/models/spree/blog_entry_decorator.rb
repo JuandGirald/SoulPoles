d1ead1ac01fd4a2fa38a2bbd1dff92f6310abeb1
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-10-29 10:46:47 -0500

    Blog fixes

--- app/models/spree/blog_entry_decorator.rb
+++ app/models/spree/blog_entry_decorator.rb
@@ -1,4 +1,6 @@
 Spree::BlogEntry.class_eval do
   has_one :blog_entry_image, :as => :viewable, :dependent => :destroy, :class_name => 'Spree::BlogEntryImage'
   accepts_nested_attributes_for :blog_entry_image, :reject_if => :all_blank
+
+  validates :blog_entry_image, presence: true
 end
